d076ac7887c4220d434939c9892fa3d0820a30ca
yusalie <71071393+yusalie@users.noreply.github.com>
2021-09-08 09:41:16 +0200

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,7 +2,7 @@
 * PHP Email Form Validation - v3.1
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
-*/
+
 (function () {
   "use strict";
 
@@ -83,3 +83,4 @@
   }
 
 })();
+*/
